d8ab48bf22d198c1eb653bac1e93a244df28ba1b
ramiz-rahman <ramiz.rahman@outlook.com>
2019-11-02 17:20:04 +0600

    feat: Create Button component
    The generic button component can be used as is or as a base
    for a more complex or specialized button.

---
+++ src/components/atoms/Button/constants.js
@@ -0,0 +1,9 @@
+export const CSS_CLASSES = {
+  ROOT: 'Button',
+  ICON: 'Button__Icon',
+  LABEL: 'Button__Label',
+  DENSE: 'Button_dense',
+  RAISED: 'Button_raised',
+  OUTLINED: 'Button_outlined',
+  UNELEVATED: 'Button_unelevated'
+};
