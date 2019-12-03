1d25ea92229a917426044b6000b0f27cdcba92b6
ramiz-rahman <ramiz.rahman@outlook.com>
2019-12-04 03:59:59 +0600

    feat: Create Switch component
    The Switch component is used to toggle the state of a single
    setting on or off.

---
+++ src/components/atoms/Switch/index.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './style.css';
+
+class Switch extends Component {
+  state = {
+    checked: false
+  };
+
+  toggleChecked = () => {
+    this.setState((prevState) => ({ checked: !prevState.checked }));
+  };
+
+  handleClick = (e) => {
+    e.preventDefault();
+    this.toggleChecked();
+    this.props.onSwitch();
+  };
+
+  render() {
+    let Switch = `Switch`;
+    if (this.state.checked) Switch += ` Switch_checked`;
+    if (this.props.className) Switch += ` ${this.props.className}`;
+    return (
+      <div className={Switch}>
+        <label className="Switch__Label" htmlFor="Switch__Thumb">
+          {this.props.label}
+        </label>
+
+        <div className="Switch__Button">
+          <div className="Switch__Track"></div>
+          <input
+            className="Switch__Thumb"
+            onClick={this.handleClick}
+            name="Switch__Thumb"
+            id="Switch__Thumb"
+            type="button"
+          ></input>
+        </div>
+      </div>
+    );
+  }
+}
+
+Switch.propTypes = {
+  onSwitch: PropTypes.func,
+  className: PropTypes.string
+};
+
+export default Switch;
