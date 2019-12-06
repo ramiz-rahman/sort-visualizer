6eda809c29b2a79fcccc029d18f67f9b0c248275
ramiz-rahman <ramiz.rahman@outlook.com>
2019-12-07 01:13:16 +0600

    feat: Create AppControls component
    The AppControls component encapsulates all the various
    controls that are required by both the TopBar and the
    AppDrawer.

---
+++ src/components/molecules/AppControls/index.js
@@ -0,0 +1,61 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import './style.css';
+
+import Button from '../../atoms/Button';
+import Switch from '../../atoms/Switch';
+import Menu from '../../molecules/Menu';
+
+const AppControls = ({
+  algorithm,
+  onAlgorithmChange,
+  onGenerateRandomArray,
+  arraySize,
+  onArraySizeChange,
+  onToggleDarkMode
+}) => {
+  return (
+    <Fragment>
+      <Menu
+        placeholder="Sort Algorithm"
+        items={[
+          'Bubble Sort',
+          'Selection Sort',
+          'Insertion Sort',
+          'Merge Sort',
+          'Quick Sort',
+          'Quick Sort 3',
+          'Heap Sort',
+          'Shell Sort'
+        ]}
+        selected={algorithm}
+        onSelect={onAlgorithmChange}
+      />
+
+      <div className="AppControls__Size">
+        <span>Size</span>
+        <Menu
+          placeholder="Array Size"
+          items={['5', '10', '25', '50', '75', '100']}
+          selected={String(arraySize)}
+          onSelect={onArraySizeChange}
+        />
+      </div>
+
+      <Button onClick={onGenerateRandomArray}>Randomize</Button>
+
+      <Switch label="Dark Mode" onSwitch={onToggleDarkMode} />
+    </Fragment>
+  );
+};
+
+AppControls.propTypes = {
+  algorithm: PropTypes.string,
+  onAlgorithmChange: PropTypes.func.isRequired,
+  onGenerateRandomArray: PropTypes.func.isRequired,
+  arraySize: PropTypes.number,
+  onArraySizeChange: PropTypes.func.isRequired,
+  onToggleDarkMode: PropTypes.func.isRequired
+};
+
+export default AppControls;
