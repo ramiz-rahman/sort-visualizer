3093795b024dfe729144c1752418cb53319e24f5
ramiz-rahman <ramiz.rahman@outlook.com>
2019-12-07 15:11:17 +0600

    feat: Create Footer
    The footer component contains links to my profile and the
    codebase.

---
+++ src/components/molecules/Footer/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './style.css';
+
+const Footer = (props) => {
+  return (
+    <footer className="Footer">
+      <section>
+        Designed and built with{' '}
+        <span className="Footer__Heart">&hearts;</span> by{' '}
+        <a
+          href="https://www.linkedin.com/in/ramiz-rahman/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="Footer__Link"
+        >
+          Ramiz Rahman
+        </a>
+      </section>
+
+      <section className="Footer__Items">
+        <a
+          href="https://www.linkedin.com/in/ramiz-rahman/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="Footer__Link"
+        >
+          Demo
+        </a>
+        <a
+          href="https://www.linkedin.com/in/ramiz-rahman/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="Footer__Link"
+        >
+          Code
+        </a>
+      </section>
+    </footer>
+  );
+};
+
+export default Footer;
